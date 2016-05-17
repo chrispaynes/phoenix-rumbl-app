dbdce8d0b017964292e0023761a65fe3a390b960
chrispaynes <username@users.noreply.github.com>
2016-05-17 16:13:28 -0500

    creates user schema and changeset function

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,5 +6,26 @@
 # structs fills in remaining values for keys without a value
 
 defmodule Rumbl.User do
-    defstruct [:id, :name, :username, :password]
+  use Rumbl.Web, :model
+      schema "users" do
+        field :name, :string
+        field :username, :string
+        field :password, :string, virtual: true
+        field :password_hash, :string
+
+        timestamps
+  end
+
+  #defines the changeset action for users
+  #pipes the username model through casts to ensure name and username are supplied
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(name username), [])
+    |> validate_length(:username, min: 1, max: 20)
+  end
+
 end
+# this code was created prior to implementing an Ecto persisted PostgresDB
+# defmodule Rumbl.User do
+#     defstruct [:id, :name, :username, :password]
+# end
