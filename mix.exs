09a58c3df1f6e9058c1f52532d83daaf351eece1
chrispaynes <username@users.noreply.github.com>
2016-05-18 21:22:12 -0500

    updates from Phoenix_HTML from 2.1 to 2.3. Adds gettext dependency

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Rumbl.Mixfile do
      version: "0.0.1",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix] ++ Mix.compilers,
+     compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      aliases: aliases,
@@ -19,7 +19,7 @@ defmodule Rumbl.Mixfile do
   def application do
     [mod: {Rumbl, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :gettext, :postgrex]]
   end
 
   # Specifies which paths to compile per environment.
@@ -33,9 +33,10 @@ defmodule Rumbl.Mixfile do
     [{:phoenix, "~> 1.1"},
      {:phoenix_ecto, "~> 2.0"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.1"},
+     {:phoenix_html, "~> 2.3"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:gettext, "~> 0.9"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
