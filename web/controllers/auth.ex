6a28220f28328627f6b7eb9bc74b6cf03eb2661e
chrispaynes <cdpaynes@gmail.com>
2016-05-25 17:55:33 -0500

    adds login function

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -14,4 +14,13 @@ defmodule Rumbl.Auth do
     user    = user_id && repo.get(Rumbl.User, user_id)
     assign(conn, :current_user, user)
   end
+
+  #receivess conn and user and stores user id in session
+  #sends session cookie back to client with new identifier
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
 end
