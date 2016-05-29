91d14083e38453a7c55f10b7ddd924a259d44f4a
chrispaynes <cdpaynes@gmail.com>
2016-05-29 14:59:23 -0500

    creates database seeds for unique video category names

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rumbl.Repo
+alias Rumbl.Category
+
+# traverses through category name list
+# retrieves the category name if already present OR
+# writes the new category to the database
+for category <- ~w(Action Drama Romance Comedy Sci-fi) do
+  Repo.get_by(Category, name: category) ||
+    Repo.insert!(%Category{name: category})
+end
