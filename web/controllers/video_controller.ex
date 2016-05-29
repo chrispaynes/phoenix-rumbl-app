c60f65def19c70fd338fa7de481420d81c6d9ca0
chrispaynes <cdpaynes@gmail.com>
2016-05-29 10:57:21 -0500

    links new and create video actions with current session user

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -3,20 +3,33 @@ defmodule Rumbl.VideoController do
 
   alias Rumbl.Video
 
+  #VideoController Pipeline
+  #checks and transforms empty strings into nil for “video” parameter data
   plug :scrub_params, "video" when action in [:create, :update]
 
-  def index(conn, _params) do
+  def index(conn, _params, user) do
     videos = Repo.all(Video)
     render(conn, "index.html", videos: videos)
   end
 
-  def new(conn, _params) do
-    changeset = Video.changeset(%Video{})
+  # creates changeset that pipes user
+  # associates current_user with :videos
+  # pipes current_user through video changeset
+  def new(conn, _params, user) do
+    changeset =
+      user
+      |> build_assoc(:videos)
+      |> Video.changeset()
+
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"video" => video_params}) do
-    changeset = Video.changeset(%Video{}, video_params)
+  # associates every newly created video to the current user
+  def create(conn, %{"video" => video_params}, user) do
+    changeset =
+      user
+      |> build_assoc(:videos)
+      |> Video.changeset(video_params)
 
     case Repo.insert(changeset) do
       {:ok, _video} ->
@@ -64,4 +77,12 @@ defmodule Rumbl.VideoController do
     |> put_flash(:info, "Video deleted successfully.")
     |> redirect(to: video_path(conn, :index))
   end
+
+  # modifies default controller action
+  # applies every actions of the current session user to the module
+  def action(conn, _)do
+    apply(__MODULE__, action_name(conn),
+      [conn, conn.params, conn.assigns.current_user])
+  end
+
 end
