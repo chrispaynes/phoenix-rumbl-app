cb8c08938bff89e50368c1a0bf0485dc74c2f1d6
chrispaynes <cdpaynes@gmail.com>
2016-05-30 10:00:46 -0500

    creates changeset error message

--- web/models/user.ex
+++ web/models/user.ex
@@ -4,7 +4,7 @@
 # structs fill in remaining values for keys without a value
 
 
-#each user has a one-to-many relationship with a video
+# each user has a one-to-many relationship with a video
 defmodule Rumbl.User do
   use Rumbl.Web, :model
       schema "users" do
@@ -17,19 +17,21 @@ defmodule Rumbl.User do
         timestamps
   end
 
-  #defines the changeset action for users and usernames
-  #pipes username model through casts to ensure name and username are supplied
-  #validates username length is between 1 and 20 characters
-  #data validation for the username
+  # defines the changeset action for users and usernames
+  # pipes username model through casts to ensure name and username are supplied
+  # validates username length is between 1 and 20 characters
+  # data validation for the username
+  # creates changeset error message when user creates duplicate username entry
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, ~w(name username), [])
     |> validate_length(:username, min: 1, max: 20)
+    |> unique_constraint(:username)
   end
 
-  #changeset to manage passwords
-  #validates password length and hashes password
-  #pipes through put_pass_hash() if changeset is valid
+  # changeset to manage passwords
+  # validates password length and hashes password
+  # pipes through put_pass_hash() if changeset is valid
   def registration_changeset(model, params) do
     model
     |> changeset(params)
@@ -38,10 +40,10 @@ defmodule Rumbl.User do
     |> put_pass_hash()
   end
 
-  #performs password hashing if the changeset is valid
-  #checks users' passwords in as secure a manner as possible.
-  #bcrypt is a password hashing function that incorporates a salt to protect against rainbow table attacks
-  #`hashpwsalt` generates a salt and hashes the password.
+  # performs password hashing if the changeset is valid
+  # checks users' passwords in as secure a manner as possible.
+  # bcrypt is a password hashing function that incorporates a salt to protect against rainbow table attacks
+  # `hashpwsalt` generates a salt and hashes the password.
   defp put_pass_hash(changeset) do
     case changeset do
       %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
