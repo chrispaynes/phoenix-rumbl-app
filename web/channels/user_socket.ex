6498edb24eebf961ab9e47ec20989a3432df2e4a
chrispaynes <cdpaynes@gmail.com>
2016-06-06 17:59:20 -0500

    Reworks def connect to verify parameters

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -25,9 +25,25 @@ defmodule Rumbl.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+
+  # sets the max time length for a valid user token
+  # this is set for 2 weeks
+  @max_age 2 * 7 * 24 * 6 * 60
+
+  # uses Phoenix.Token to verify client provided user token
+  # stores the user_id in socket.assigns and returns {:ok, socket}
+  # starts connection if a valid token provided
+  # denies connection for invalid tokens
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
+
+  def connect(_params, _socket), do: :error
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
@@ -39,6 +55,6 @@ defmodule Rumbl.UserSocket do
   #     Rumbl.Endpoint.broadcast("users_socket:" <> user.id, "disconnect", %{})
   #
   # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 
 end
