0ae21d55af8519ea074ff663a63c18126ffd3572
chrispaynes <cdpaynes@gmail.com>
2016-06-06 18:35:03 -0500

    Adds semicolons

--- web/static/js/player.js
+++ web/static/js/player.js
@@ -9,11 +9,12 @@ let Player = {
     // seeks out a current time in video
   init(domId, playerId, onReady){
     window.onYouTubeIframeAPIReady = () => {
-      this.onIframeReady(domId, playerId, onReady)
-    }
-    let youtubeScriptTag = document.createElement("script")
-    youtubeScriptTag.src = "//www.youtube.com/iframe_api"
-    document.head.appendChild(youtubeScriptTag)
+      this.onIframeReady(domId, playerId, onReady);
+    };
+
+    let youtubeScriptTag = document.createElement("script");
+    youtubeScriptTag.src = "//www.youtube.com/iframe_api";
+    document.head.appendChild(youtubeScriptTag);
   },
   onIframeReady(domId, playerId, onReady){
     this.player = new YT.Player(domId, {
@@ -24,11 +25,16 @@ let Player = {
         "onReady": (event => onReady(event) ),
         "onStateChange": (event => this.onPlayerStateChange(event) )
       }
-    })
+    });
   },
-  onPlayerStateChange(event){ },
-  getCurrentTime(){ return Math.floor(this.player.getCurrentTime() * 1000) },
-  seekTo(millsec){ return this.player.seekTo(millsec / 1000) }
-}
 
-export default Player
+  onPlayerStateChange(event) { },
+  getCurrentTime() {
+    return Math.floor(this.player.getCurrentTime() * 1000);
+  },
+  seekTo(millsec) {
+    return this.player.seekTo(millsec / 1000);
+  }
+};
+
+export default Player;
