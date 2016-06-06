c0331c0f1f6d2978ddf94c73f6b4a484204d33f6
chrispaynes <cdpaynes@gmail.com>
2016-06-06 17:46:28 -0500

    Adds missing semicolons

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,16 +12,15 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
     // import "deps/phoenix_html/web/static/js/phoenix_html" DEPRECATED
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-import socket from "./socket"
-import Video from "./video"
+import socket from "./socket";
+import Video from "./video";
 
 // initializes video with a socket connection
-Video.init(socket, document.getElementById("video"))
-
+Video.init(socket, document.getElementById("video"));
