1d6d0a32f709c898a62f847b8121a0e8382a7ac5
chrispaynes <cdpaynes@gmail.com>
2016-06-07 18:31:39 -0500

    Cleans up indentation

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,15 +1,15 @@
 defmodule Rumbl.UserView do
-    use Rumbl.Web, :view
-    alias Rumbl.User
+  use Rumbl.Web, :view
+  alias Rumbl.User
 
-    def first_name(%User{name: name}) do
-        name 
-        |> String.split(" ")
-        |> Enum.at(0)
-    end
+  def first_name(%User{name: name}) do
+      name 
+      |> String.split(" ")
+      |> Enum.at(0)
+  end
 
-    # renders user.json to view user's who submit annotation in chat
-    def render("user.json", %{user: user}) do
-      %{id: user.id, username: user.username}
-    end 
+  # renders user.json to view user's who submit annotation in chat
+  def render("user.json", %{user: user}) do
+    %{id: user.id, username: user.username}
+  end 
 end
