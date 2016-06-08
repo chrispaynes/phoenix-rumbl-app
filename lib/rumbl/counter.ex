d150dc850cece66660da090cc06f8078066b1c47
chrispaynes <cdpaynes@gmail.com>
2016-06-08 17:50:05 -0500

    Adds def handle_info() callback

--- lib/rumbl/counter.ex
+++ lib/rumbl/counter.ex
@@ -18,8 +18,22 @@ defmodule Rumbl.Counter do
     GenServer.start_link(__MODULE__, initial_val)
   end
 
+  # sends a "tick" message every 1000ms
   def init(initial_val) do
+    Process.send_after(self, :tick, 1000)
     {:ok, initial_val}
+  end
+
+  # raises an error if counter val is less than 0
+  # server is restarted to its initial state after the crash
+  def handle_info(:tick, val) when val <= 0, do: raise "boom"
+
+  # processes def init()'s' ticks
+  # simulates a countdown
+  def handle_info(:tick, val) do
+    IO.puts "tick #{val}"
+    Process.send_after(self, :tick, 1000)
+    {:noreply, val - 1}
   end
 
   def handle_cast(:inc, val) do
