5003c5991e2d08288db7c0e307e25cab4f5bdb52
chrispaynes <cdpaynes@gmail.com>
2016-06-08 16:32:00 -0500

    Adds the Counter server to the Rumbl's application supervision tree

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -3,21 +3,24 @@ defmodule Rumbl do
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
+  # Starts the endpoint when the application starts
+  # A child is started by the Elixir App
+  # If hard coding a repo, comment out the ELixir supervisor and the worker.  
   def start(_type, _args) do
     import Supervisor.Spec, warn: true
 
     children = [
-  #     # Start the endpoint when the application starts
-  #     # THIS WAS COMMENTED OUT, FILE IS NO LONGER AN ECTO REPO =>
-  # If hard coding a repo, comment over the ELixir supervisor and the worker.
-  supervisor(Rumbl.Endpoint, []),
-  worker(Rumbl.Repo, []),
-  # #     # Here you could define other workers and supervisors as children
-  # #     # worker(Rumbl.Worker, [arg1, arg2, arg3]),
+      supervisor(Rumbl.Endpoint, []),
+      supervisor(Rumbl.Repo, []),
+      worker(Rumbl.Counter, [5]),
+      # Here you could define other workers and supervisors as children
+      # worker(Rumbl.Worker, [arg1, arg2, arg3]),
     ]
 
+    # this details the OTP Supervision Strategy
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
+    # ":one_for_one" indicates 1 child will be restarted if 1 dies 
     opts = [strategy: :one_for_one, name: Rumbl.Supervisor]
     Supervisor.start_link(children, opts)
   end
