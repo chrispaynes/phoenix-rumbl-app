eebed56c2ca9845d80ccf665842fb598a588ac7a
chrispaynes <cdpaynes@gmail.com>
2016-06-08 22:35:58 -0500

    Refactors to create Wolfram user seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,12 +11,15 @@
 # and so on) as they will fail if something goes wrong.
 
 alias Rumbl.Repo
-alias Rumbl.Category
+# alias Rumbl.Category
+alias Rumbl.User
+
+Repo.insert!(%User{name: "Wolfram", username: "wolfram"})
 
 # traverses through category name list
 # retrieves the category name if already present OR
 # writes the new category to the database
-for category <- ~w(Action Drama Romance Comedy Sci-fi) do
-  Repo.get_by(Category, name: category) ||
-    Repo.insert!(%Category{name: category})
-end
+# for category <- ~w(Action Drama Romance Comedy Sci-fi) do
+#   Repo.get_by(Category, name: category) ||
+#     Repo.insert!(%Category{name: category})
+# end
